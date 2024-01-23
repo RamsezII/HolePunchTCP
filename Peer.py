3851cdefe34700a5470637740b57428be269aaef
ram16 <thomass.montaine@gmail.com>
2024-01-23 01:32:35 +0100

    ah bordel

--- Peer.py
+++ Peer.py
@@ -10,9 +10,6 @@ if __name__ == "__main__":
     local_address = joinPublic.getsockname()
     print("Local address:", local_address)
 
-    hostDirect = TCP.TCP()
-    hostDirect.bind(local_address)
-
     print("Connected to server")
 
     myPublicHost = joinPublic.recv_str()
@@ -41,6 +38,9 @@ if __name__ == "__main__":
     thread1.start()
 
     def accept_incoming():
+        hostDirect = TCP.TCP()
+        hostDirect.bind(local_address)
+
         osock = hostDirect.accept()
         print("other host connected:", osock.getpeername())
         msg = TCP.recv_str(osock)
